f46bd04f3af500a6fc1be550e0231a028f5712d8
ramxv <ramxv.singh@gmail.com>
2024-03-31 09:30:28 -0500

    Create user

--- includes/navbar.php
+++ includes/navbar.php
@@ -9,11 +9,17 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
-        <li class="nav-item">
+        <!-- <li class="nav-item">
           <a class="nav-link" href="#">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="./add.php">Add Contact</a>
+        </li> -->
+        <li class="nav-item">
+          <a class="nav-link" href="register.php">Register</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./add.php">Login</a>
         </li>
       </ul>
     </div>
--- sql/setup.sql
+++ sql/setup.sql
@@ -4,6 +4,16 @@ CREATE DATABASE contacts_app;
 
 USE contacts_app;
 
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255) UNIQUE,
+    password VARCHAR(255)
+);
+
+INSERT INTO users (name, email, password) VALUES ("Ram", "ram@test.com", "1234");
+
+
 CREATE TABLE contacts(
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255),
